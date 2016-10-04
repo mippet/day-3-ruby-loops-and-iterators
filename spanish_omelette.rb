64d2bce522981e918c1a9edcc04c1a83d2bdb136
p.luitjes <pimluitjes@gmail.com>
2016-10-04 15:43:22 +0200

    Define the steps actions

--- spanish_omelette.rb
+++ spanish_omelette.rb
@@ -54,3 +54,53 @@ steps = [
   { description: "When almost set, invert on a plate and slide back into the pan", action: "generic_recipe_step" },
   { description: "Cook a few more minutes and slide on to a plate and cool for 10 minutes before serving", action: "generic_recipe_step" }
 ]
+
+print_divider
+puts "see below the differtent steps to follow"
+
+steps.each_with_index do |step, index|
+  puts (index + 1).to_s + ")" + step[:description]
+end
+
+def generic_recipe_step
+  puts "on it!"
+  print_progress_bar
+end
+
+def scrape_potatoes
+  counter = 0
+  while counter < NUM_POTATOES
+    counter += 1
+    print "scraping potato #{counter}"
+    print print_progress_bar
+  end
+end
+
+def cut_potatoes
+  counter = 0
+  while counter < NUM_POTATOES
+    counter += 1
+    print "cutting potato #{counter} into thick slices"
+    print_progress_bar
+  end
+end
+
+def add_potatoes_to_pan
+  for counter in 1..NUM_POTATOES
+    print "adding potato #{counter} to the pan"
+    print_progress_bar
+  end
+end
+
+def break_eggs
+  counter = 0
+  while counter < NUM_EGGS
+    counter += 1
+    print "breaking egg #{counter}"
+    print_progress_bar
+  end
+end
+
+
+
+  end
